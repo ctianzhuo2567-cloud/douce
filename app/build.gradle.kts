9a369086950d751272cad5bae561ca02ef3441a1
ctianzhuo2567-cloud <ctianzhuo2567@gmail.com>
2026-09-11 21:56:54 +0800

    Bump development version to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pindou.patternbook"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.5.0"
+        versionCode = 7
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
